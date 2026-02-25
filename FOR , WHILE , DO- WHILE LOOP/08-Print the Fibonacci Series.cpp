df4f1c85354a666a172d54c884831831204562af
Asadullah <asadullahyounas1437@gmail.com>
2026-02-25 22:57:52 +0500

    uploaded the fibonacci serires program

---
+++ FOR , WHILE , DO- WHILE LOOP/08-Print the Fibonacci Series.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+ int main()
+ {
+ 	int n, x,y,z;
+ 	cout<<" Enter the number :"<<endl;
+ 	cin>>n;
+ 	x=0;
+ 	y=1;
+ 	z=0;
+ 	while( z<=n)
+{
+
+ 		cout<<"  "<<z<<endl;
+ 		x=y;
+ 		y=z;
+ 		z=x+y;
+	 }
+	 
+	 return 0;
+ }
